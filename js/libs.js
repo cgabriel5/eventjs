e4e6a5bff1b3728407b9095ba5fea0849a44c8db
cgabriel5 <carlosgabriel71@yahoo.com>
2017-08-11 14:35:23 -0700

    chore: Update libs.js
    Update the entire 3rd party library file due to updating funnel.js

--- js/libs.js
+++ js/libs.js
@@ -688,10 +688,22 @@
         window.FastClick = FastClick;
     }
 })();
-(function() {
+// IIFE start
+(function(window) {
     "use strict";
     var library = (function() {
         // =============================== Helper Functions
+        /**
+         * @description [Generates a simple ID containing letters and numbers.]
+         * @param  {Number} length [The length the ID should be. Max length is 22 characters]
+         * @return {String}        [The newly generated ID.]
+         * @source {http://stackoverflow.com/a/38622545}
+         */
+        function id(length) {
+            return Math.random()
+                .toString(36)
+                .substr(2, length);
+        }
         /**
          * @description [Returns index of given value in provided array.]
          * @param  {Array}    array [The array to check against.]
@@ -714,6 +726,14 @@
             return -~index(iterable, value);
         }
         /**
+         * @description [Checks if the provided index exists.]
+         * @param  {Number} index [The index (number) to check.]
+         * @return {Boolean}       [False if -1. Otherwise, true.]
+         */
+        function indexed(index) {
+            return -~index ? true : false;
+        }
+        /**
          * @description [Makes an Array from an array like object (ALO). ALO must have a length property
          *               for it to work.]
          * @param  {ALO} alo [The ALO.]
@@ -727,20 +747,43 @@
             return true_array;
         }
         /**
-         * @description [Returns the data type of the object passed or compares against the data type
-         *               if provided.]
-         * @param  {Any} object         [The object to check.]
-         * @param  {String} comparative [The data type to compare against. This parameter is optional.]
-         * @return {String|Boolean}     [The data type of the object as a string. Or a boolean
-         *                               indicating the state of the comparison.]
+         * @description [Returns the data type of the provided object.]
+         * @param  {Any} object [The object to check.]
+         * @return {String}    [The data type of the checked object.]
          */
-        function dtype(object, comparative) {
+        var dtype = function(object) {
             // will always return something like "[object {type}]"
-            var check = Object.prototype.toString.call(object)
+            return Object.prototype.toString.call(object)
                 .replace(/(\[object |\])/g, "")
                 .toLowerCase();
-            return (!comparative) ? check : (check === comparative.toLowerCase());
-        }
+        };
+        /**
+         * @description [Check if the provided object is of the provided data types.]
+         * @param  {Any} object [The object to check.]
+         * @param  {String}  types  [The allowed data type the object may be.]
+         * @return {Boolean}        [Boolean indicating whether the object is of the
+         *                           allowed data types.]
+         */
+        dtype.is = function(object, types) {
+            // get the object type
+            var type = this(object);
+            // prepare the types
+            types = "|" + types.toLowerCase()
+                .trim() + "|";
+            // check if the object's type is in the list
+            return Boolean(-~types.indexOf("|" + type + "|"));
+        };
+        /**
+         * @description [Check if the provided object is not of the provided data types.]
+         * @param  {Any} object [The object to check.]
+         * @param  {String}  types  [The prohibited data types.]
+         * @return {Boolean}        [Boolean indicating whether the object is not of the
+         *                           allowed data types.]
+         */
+        dtype.isnot = function(object, types) {
+            // return the inverse of the is method
+            return !this.is(object, types);
+        };
         /**
          * @description [A class wrapper. Creates a class based on provided object containing class constructor__ and methods__.
          *               If class needs to extend another, provide it under the extend__ property.]
@@ -942,7 +985,12 @@
                         if (!parts[1]) elements = elements.concat([cid]);
                         // else apply the filter and add all returned (filtered) elements to array
                         else elements = elements.concat(to_array(this[parts[1]]([cid]))); // i.e. -> this.all()
-                    } else if (/^html/.test(data_type)) { // the point is a DOMElement
+                    } else if (/^(html|text|comment)/.test(data_type)) { // HTMLElement/TextNode/Comment
+                        // **Note**: possibly use the element.nodeType attribute to determine
+                        // whether an element, text, comment, or the document was passed in instead?
+                        // [https://developer.mozilla.org/en-US/docs/Web/API/Node/nodeType]
+                        // For now only HTMLElements, Text/Comments nodes can be passed in.
+                        //
                         // **Note: the selector can also take in raw element nodes (elements)
                         // it can take N amount of DOM nodes. for example, using
                         // Google Chrome's console this is a valid use case:
@@ -1618,13 +1666,14 @@
     // =============================== Attach Library To Global Scope
     // add to global scope for ease of use
     // use global app var or create it if not present
-    var app = (window.app || (window.app = {}));
+    var app = window.app || (window.app = {});
     // get the libs object from within the app object
     // if it does not exist create it
-    var libs = (app.libs || (app.libs = {}));
+    var libs = app.libs || (app.libs = {});
     // add the library to the libs object
     libs.Funnel = library;
-})();
+    // IIFE end
+})(window);
 (function() {
     // initialize 3rd-party libraries
     // init FastClickJS
